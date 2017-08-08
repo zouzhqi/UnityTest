82532088191229d316f637dd9517d989362ae95e
zouzhqi <zouzhqi@qq.com>
2017-08-08 16:28:32 +0800

    1

---
+++ Assets/test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        XLua.LuaEnv luaenv = new XLua.LuaEnv();
+        luaenv.DoString("CS.UnityEngine.Debug.Log('hello Unity')");
+        luaenv.Dispose();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
